fe0270745cd8eb749df6d4590da014a4810eec9d
Thirupathi <thirupathi@users.noreply.github.com>
2026-07-11 10:18:35 +0530

    Fix: auto-detect Python version for PYTHONPATH

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess, sys, os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # HiddenCloud installs to .local/ but /usr/local/bin/python doesn't look there
-local_site = os.path.join(os.getcwd(), '.local', 'lib', 'python3.12', 'site-packages')
+local_site = os.path.join(os.getcwd(), '.local', 'lib', f'python{sys.version_info.major}.{sys.version_info.minor}', 'site-packages') #SN
 env = os.environ.copy()
 if os.path.isdir(local_site):
     env['PYTHONPATH'] = local_site + os.pathsep + env.get('PYTHONPATH', '')
