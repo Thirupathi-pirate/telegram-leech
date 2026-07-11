333757ecd4df2c6dfe09c46666abe00bd2e742e3
Thirupathi <thirupathi@users.noreply.github.com>
2026-07-11 10:21:07 +0530

    Fix: force setuptools<71 with --target for pkg_resources

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import subprocess, sys, os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-# HiddenCloud installs to .local/ but /usr/local/bin/python doesn't look there
-local_site = os.path.join(os.getcwd(), '.local', 'lib', f'python{sys.version_info.major}.{sys.version_info.minor}', 'site-packages') #SN
+# HiddenCloud installs to ~/.local/lib/pythonX.Y/site-packages
+pyver = f'python{sys.version_info.major}.{sys.version_info.minor}'
+local_site = os.path.expanduser(f'~/.local/lib/{pyver}/site-packages')
+if not os.path.isdir(local_site):
+    local_site = os.path.join(os.getcwd(), '.local', 'lib', pyver, 'site-packages')
 env = os.environ.copy()
 if os.path.isdir(local_site):
     env['PYTHONPATH'] = local_site + os.pathsep + env.get('PYTHONPATH', '')
+# Ensure pkg_resources is importable from the local site-packages
+subprocess.run([sys.executable, '-m', 'pip', 'install', '--target', local_site, 'setuptools<71'], check=False, env=env)
 subprocess.run([sys.executable, '-m', 'bot'], check=True, env=env)
